5816c2637c8dcbb8c2a9489ae0954d7dc8f5f842
Sheinid <sheinid@ya.ru>
2022-12-10 19:02:03 +0300

    practice 1

---
+++ practice-1/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void DigitClicked(object sender, EventArgs e)
+	{
+		DisplayLabel.Text += (sender as Button).Text;
+	}
+	private void CrossClicked(object sender, EventArgs e)
+	{
+		DisplayLabel.Text = "";
+	}
+
+	private void CheckMarkClicked(object sender, EventArgs e)
+	{
+		string _password = "1234";
+
+		if (DisplayLabel.Text == _password)
+		{
+			DisplayLabel.Text = "Your Password is correct!";
+
+			foreach (var btn in Container.Children.OfType<Button>())
+			{
+				btn.IsEnabled = false;
+			}
+		}
+		else
+		{
+            CrossClicked(sender, e);
+        }
+    }
+}
+
